95606c732cc68bcbe1984169603d1272c7a6ee69
tudoistube <tudoistube@ymail.com>
2017-12-01 22:48:15 +0900

    zvuejs2Start

---
+++ vue-shopping-list/zscript_046p.js
@@ -0,0 +1,10 @@
+var data = {
+  items: [{ text: 'Bananas', checked: true },
+          { text: 'Apples', checked: false }],
+  title: 'My Shopping List'
+};
+
+new Vue({
+  el: '#app',
+  data: data
+});
