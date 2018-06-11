bf9388801f0b1fea3bac0a25d6f3aec78868e378
tudoistube <tudoistube@ymail.com>
2018-06-11 15:45:04 +0900

    36.App 제작-글수정-파일명 변경, 내용저장

--- main.js
+++ main.js
@@ -135,6 +135,29 @@ var app = http.createServer(function(request,response){
           response.end(template);
         });
       });
+    } else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs .parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        console.log(`id : ${post.id}, title : ${post.title}, description: ${post.description}`);
+        fs.rename(`data/${post.id}`, `data/${post.title}`, (err) => {
+          if (err) throw err;
+          fs.writeFile(`./data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          })
+          console.log('Rename complete!');
+        });
+        /*;
+        */
+      });
+
     } else {
       response.writeHead(404);
       response.end('Not found');
