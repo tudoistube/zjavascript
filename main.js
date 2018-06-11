c9f31f3c91636a9304592382ab76e0ef032147da
tudoistube <tudoistube@ymail.com>
2018-06-11 09:16:25 +0900

    26.App 제작-함수를 이용해서 정리 정돈하기

--- main.js
+++ main.js
@@ -2,11 +2,39 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHtml(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist){
+
+    var list = '<ul>';
+    var i = 0;
+    while(i < filelist.length){
+      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      i = i + 1;
+    }
+    list = list+'</ul>';
+    return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
     console.log(`queryData.id : ${queryData.id}, pathname : ${pathname}`);
 
     if(pathname === '/'){
@@ -17,30 +45,10 @@ var app = http.createServer(function(request,response){
 
           var title = 'Welcome';
           var description = 'Hello, Node.js';
+          var body = `<h2>${title}</h2> ${description}`;
+          var list = templateList(filelist);
+          var template = templateHtml(title, list, body);
 
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list+'</ul>';
-
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
           response.writeHead(200);
           response.end(template);
         });
@@ -48,31 +56,12 @@ var app = http.createServer(function(request,response){
       } else {
         fs.readdir('./data', function(error, filelist){
 
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list+'</ul>';
-
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var body = `<h2>${title}</h2> ${description}`;
+            var list = templateList(filelist);
+            var template = templateHtml(title, list, body);
+
             response.writeHead(200);
             response.end(template);
           });
