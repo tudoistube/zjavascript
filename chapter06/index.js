2e2c6ea9521788b4953a27988926e34f69354556
tudoistube <tudoistube@ymail.com>
2018-06-17 14:00:57 +0900

    ~109p.HR앱을 위한 노드서버 구축

--- chapter06/index.js
+++ chapter06/index.js
@@ -1,4 +1,9 @@
 var http = require('http');
+var colors = require('colors');
+var employeeService = require('./lib/employees');
+var responder = require('./lib/responseGenerator');
+var staticFile = responder.staticFile('/public');
+
 
 http.createServer(function (req, res) {
   // A parsed url to work with in case there are parameters
@@ -15,22 +20,33 @@ http.createServer(function (req, res) {
     return res.end(req.method + ' is not implemented by this server.');
   }
 
+  if (_url = /^\/employees$/i.exec(req.url)) {
+    employeeService.getEmployees(function (error, data) {
+      if (error) {
+        return responder.send500(error, res);
+      }
+      return responder.sendJson(data, res);
+    });
+  } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
+    employeeService.getEmployee(_url[1], function (error, data) {
+      if (error) {
+        return responder.send500(error, res);
+      }
 
-    if (_url = /^\/employees$/i.exec(req.url)) {
-      res.writeHead(200);
-      return res.end(req.method + ' employees list');
+      if (!data) {
+        return responder.send404(res);
+      }
 
-    } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
-      res.writeHead(200);
-      return res.end(req.method + ' an employee info');
-
-    }
-    else {
-      // try to send the static file
-      res.writeHead(200);
-      res.end('static file maybe');
-    }
-
+      return responder.sendJson(data,res);
+    });
+  }
+  else {
+    // try to send the static file
+    res.writeHead(200);
+    res.end('static file maybe');
+    // added by jrogue@gmail.com for programming exercises
+    // staticFile(req.url, res);
+  }
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
