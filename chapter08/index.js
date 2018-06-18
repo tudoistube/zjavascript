b61fe9c21918bb65f89e8582eafc152127a6bf11
tudoistube <tudoistube@ymail.com>
2018-06-18 16:02:32 +0900

    132p.팀하나를 데이터베이스에 추가하기

--- chapter08/index.js
+++ chapter08/index.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var db = mongoose.connection;
+var dbUrl = 'mongodb://dbuser0100:dbuser0100@ds163850.mlab.com:63850/humanresources';
+
 
 var TeamSchema = new Schema({
   name: {
@@ -7,32 +10,32 @@ var TeamSchema = new Schema({
     required: true
   }
 });
+var Team = mongoose.model('Team', TeamSchema);
 
-var EmployeeSchema = new Schema({
-  name: {
-    first: {
-      type: String,
-      required: true
-    },
-    last: {
-      type: String,
-      required: true
-    }
-  },
-  team: {
-    type: Schema.Types.ObjectId,
-    ref: 'Team'
-  },
-  image: {
-    type: String,
-    default: 'images/user.png'
-  },
-  address: {
-    lines: {
-      type: [String]
-    },
-    postal: {
-      type: String
-    }
+db.on('error', function () {
+  console.log('there was an error communicating with the database');
+});
+
+mongoose.connect(dbUrl, function (err) {
+  if (err) {
+    return console.log('there was a problem connecting to the database!' + err);
   }
+  console.log('connected!');
+
+  var team = new Team({name: 'Product Development'});
+
+  team.save(function(err, data){
+    if(err){
+      console.log('err in save : ${err}');
+    }else{
+      console.dir(data);
+    }
+    db.close();
+    process.exit();
+
+  })
+
+
+
+
 });
